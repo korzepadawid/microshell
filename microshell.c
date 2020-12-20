4735b02b651e323df53060b08be147176822f8e1
korzepadawid <korzepadawid@yahoo.com>
2020-12-20 13:17:23 +0100

    added cd and execvp

--- microshell.c
+++ microshell.c
@@ -3,33 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define EXIT "exit"
 #define HELP "help"
 #define CLEAR "clear"
+#define CD "cd"
 
 #define BUFFER 256
 
+/*Shell programs*/
+
 void help();
 void clear();
+void cd(char *args[]);
+int execute(char *args[]);
 
-const char *get_current_path();
-void parse_args(char *args[], char command[]);
+/*Helpers*/
+
+const char *path();
+void parse_args(char *args[], char command[], int *args_count);
 
 int main()
 {
-
+    clear();
     while (true)
     {
+
         char *args[BUFFER];
+        int args_count = 0;
         char command[BUFFER];
 
-        printf("[%s] \n$ ", get_current_path());
+        printf("[%s] \n$ ", path());
         fgets(command, sizeof command, stdin);
-        parse_args(args, command);
+        parse_args(args, command, &args_count);
 
-        if (strcmp(args[0], EXIT) == 0)
+        if (args[0] == NULL)
+        {
+            continue;
+        }
+        else if (strcmp(args[0], EXIT) == 0)
         {
             exit(0);
         }
@@ -41,16 +55,23 @@ int main()
         {
             clear();
         }
+        else if (strcmp(args[0], CD) == 0)
+        {
+            cd(args);
+        }
         else
         {
-            printf("Unknown command, type help if you got lost.\n");
+            if (execute(args) < 0)
+            {
+                printf("%s, type help if you got lost. \n", strerror(errno));
+            }
         }
     }
 
     exit(EXIT_SUCCESS);
 }
 
-const char *get_current_path()
+const char *path()
 {
     static char current_path[BUFFER];
     getcwd(current_path, sizeof current_path);
@@ -62,7 +83,7 @@ const char *get_current_path()
     return current_path;
 }
 
-void parse_args(char *args[], char command[])
+void parse_args(char *args[], char command[], int *args_count)
 {
     int i = 0;
     char *temp;
@@ -73,11 +94,50 @@ void parse_args(char *args[], char command[])
         temp = strtok(NULL, " \n\t");
     }
     args[i] = NULL;
+    *args_count = i - 1;
 }
 
 void clear()
 {
     printf("\e[1;1H\e[2J");
+}
+
+void cd(char *args[])
+{
+    if (args[1] == NULL)
+    {
+        chdir(".");
+    }
+    else if (strcmp(args[1], "..") == 0)
+    {
+        chdir("..");
+    }
+    else if (strcmp(args[1], "~") == 0)
+    {
+        chdir(getenv("HOME"));
+    }
+    else if (chdir(args[1]) == -1)
+    {
+        printf("Fatal error: %s\n", strerror(errno));
+    }
+    else
+    {
+        chdir(args[1]);
+    }
+}
+
+int execute(char *args[])
+{
+    int pid, result;
+    if ((pid = fork()) == 0)
+    {
+        result = execvp(args[0], args);
+    }
+    else
+    {
+        wait(NULL);
+    }
+    return result;
 }
 
 void help()
@@ -98,5 +158,6 @@ void help()
     printf("clear  \t\tThere will be a cool info.\n");
     printf("help   \t\tThere will be a cool info.\n");
     printf("exit   \t\tThere will be a cool info.\n");
+    printf("cd     \t\tThere will be a cool info.\n");
     printf("\n\n");
 }
