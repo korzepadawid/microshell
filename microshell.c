0f17f189ae7136520c5a2d29e7361eff83127ef4
korzepadawid <korzepadawid@yahoo.com>
2021-01-12 20:16:44 +0100

    removed unused function

--- microshell.c
+++ microshell.c
@@ -51,7 +51,6 @@
 void execute(char *argv[]);
 const char *path();
 const char *user();
-char *substring(char *string, int position, int length);
 void parse_args(char *argv[], int *argc, char *cmd);
 bool exists(char *filename);
 bool is_dir(char *path);
@@ -182,30 +181,6 @@ const char *path()
         exit(EXIT_FAILURE);
     }
     return current_path;
-}
-
-char *substring(char *string, int position, int length)
-{
-    char *p;
-    int c;
-
-    p = malloc(length + 1);
-
-    if (p == NULL)
-    {
-        printf("Unable to allocate memory.\n");
-        exit(1);
-    }
-
-    for (c = 0; c < length; c++)
-    {
-        *(p + c) = *(string + position - 1);
-        string++;
-    }
-
-    *(p + c) = '\0';
-
-    return p;
 }
 
 void parse_args(char *argv[], int *argc, char *cmd)
